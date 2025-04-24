7adcb839a4e7054625f643d43d867fa759919b30
theo.dip <theo.dipauli@cs2i-lorient.fr>
2025-04-24 14:54:57 +0200

    update index

--- app/src/Controller/DefaultController.php
+++ app/src/Controller/DefaultController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Post;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,9 +11,14 @@ use Symfony\Component\Routing\Attribute\Route;
 final class DefaultController extends AbstractController
 {
     #[Route('/', name: 'app')]
-    public function index(): Response
+    public function index(EntityManagerInterface $em): Response
     {
+        $posts = $em->getRepository(Post::class)->findBy(
+            ['status' => 1],
+            ['id' => 'ASC']
+        );
         return $this->render('default/index.html.twig', [
+            'posts' => $posts
         ]);
     }
 
--- app/src/Entity/Post.php
+++ app/src/Entity/Post.php
@@ -7,7 +7,7 @@ use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: PostRepository::class)]
-#[ORM\Table("post",schema:"blog")]
+#[ORM\Table("posts",schema:"blog")]
 class Post
 {
     #[ORM\Id]
